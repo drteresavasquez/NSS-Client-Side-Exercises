3593793fb7ef0e342f05cd35601ca0e60b4045b0
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-06-27 09:37:40 -0500

    added chickenmonkey

--- book2/Chapter5-Functions&Logic/ChickenMonkey.js
+++ book2/Chapter5-Functions&Logic/ChickenMonkey.js
@@ -1,17 +1,16 @@
 console.log("HERE!");
 
-for(let i = 1; i <= 100; i++){
-    if(i % 35 === 0){
+for (let i = 1; i <= 100; i++) {
+    if (i % 35 === 0) {
         console.log("ChickenMonkey")
-    }
-    else if(i % 5 ===0){
+    } else if (i % 5 === 0) {
         console.log("Chicken")
-    }else if(i % 7===0){
+    } else if (i % 7 === 0) {
         console.log("Monkey")
-    }else{
+    } else {
         console.log(i)
     }
 
     // TERINARY OPERATOR
-    // i % 35 === 0 ? console.log("ChickenMonkey") : i % 5 === 0 ? console.log("Chicken") : i%7===0 ? console.log("Monkey") : console.log(i)
+    i % 35 === 0 ? console.log("ChickenMonkey") : i % 5 === 0 ? console.log("Chicken") : i % 7 === 0 ? console.log("Monkey") : console.log(i)
 }
