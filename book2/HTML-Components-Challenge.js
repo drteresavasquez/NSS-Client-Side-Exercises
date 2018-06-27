dfe4398c9ec36aa05a0793a991deb24dc83b1050
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-06-27 09:20:34 -0500

    updated

--- book2/HTML-Components-Challenge.js
+++ book2/HTML-Components-Challenge.js
@@ -1,12 +1,17 @@
 console.log("HERE!");
 
-// CHALLENGE
+// MAIN CHALLENGE
 // document.querySelector('article').innerHTML = `Mission: WORLD DOMINATION!`
 
 // ADVANCED CHALLENGE
 // In JavaScript, there is a method that creates a new attribute node for any DOM element. Discover that method and use it to create a custom attribute named congressional-district and its value should be your congressional district number. Then attach that attribute to the <article> container you made for your representative component.
 let node = document.querySelector('article');
 let attr = document.createAttribute("congressional-district");
-attr.value = 5;
+// attributes are strings
+attr.value = "5";
 node.setAttributeNode(attr);
+
+// check elements in the console, your article element should now have the added attribute
+/* <article class="main" congressional-district="5">...</article> */
+
 console.log(node.getAttribute("congressional-district")); 
