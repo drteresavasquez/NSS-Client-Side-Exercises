986f1904910daa37c8739f78defd58aff12d1c9e
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-06-27 15:04:58 -0500

    did the disarray challenge

--- book2/Chapter5-Functions&Logic/cookout.js
+++ book2/Chapter5-Functions&Logic/cookout.js
@@ -26,7 +26,7 @@ const steak = {
 	cooked: false,
 };
 
-const uncookedFood = [hamburger, zucchini, chickenBreast, corn, steak];
+let uncookedFood = [hamburger, zucchini, chickenBreast, corn, steak];
 const cookedFood = [];
 
 // This exercise does not require you to alter this function. But read it. See if you can follow what it takes in, executes, and returns.
@@ -43,17 +43,21 @@ const grill = currentObject => {
 // 1. Write a for loop that iterates over the objects in the uncookedFood array. Ensure the loop only iterates to the end of the array.
 for(let i = 0; i < uncookedFood.length; i++){
     // 2. Console.log() the results to ensure your loop is returning Food objects.
-    console.log(uncookedFood[i]);
+    // console.log(uncookedFood[i]);
 
     // 3. Save uncookedFood[i] into a variable named 'currentObject'.
     let currentObject = uncookedFood[i];
 
     // 4. Now, add a line that calls the grill() function and passes in the current item being iterated over.
     grill(currentObject)
-    
+
     // DELETE THE ITEMS FROM THE ARRAY AS THEY ARE COOKED...
-    delete uncookedFood[i]
-    console.log(uncookedFood);
+    // delete uncookedFood[i]
+
+    // STILL WORKING ON THIS...THE ARRAY IS SHIFTING SO THE LENGTH IS BEING UPDATED WITH EACH PASS...
+    uncookedFood = uncookedFood.filter(item => !cookedFood.includes(item))
+    console.log("UNCOOKED STUFF", uncookedFood);
+    console.log("COOKED STUFF", cookedFood);
 }
 // If the loop successfully iterates over the array and successfully executes the grill() function on each object. This console log should appear as an array that has each object's 'cooked' key now shows true.
 //********** LOOP ANSWER END *******************//
